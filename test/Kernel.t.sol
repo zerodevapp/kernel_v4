fa51ff047896038e5f6697eef517aa0de12219cc
taek <leekt216@gmail.com>
2025-10-14 09:56:57 +0900

    Revert "Fix/tob kernelv4 14"

--- test/Kernel.t.sol
+++ test/Kernel.t.sol
@@ -33,12 +33,10 @@ contract KernelTest is
     KernelSelectorTest,
     KernelHookTest
 {
-    KernelUUPS uups;
-
     function setUp() external {
         ep = EntryPointLib.deploy();
 
-        uups = new KernelUUPS(ep);
+        KernelUUPS uups = new KernelUUPS(ep);
         KernelImmutableECDSA immutableEcdsa = new KernelImmutableECDSA(ep);
         factory = new KernelFactory(uups, immutableEcdsa);
         helper = new KernelHelper();
@@ -51,15 +49,6 @@ contract KernelTest is
         signer = new MockSigner();
         permissionId = bytes20(keccak256(abi.encodePacked("Hello world")));
         _initialize();
-    }
-
-    function test_implementation_revert_on_initialize() external {
-        rootValidator = new MockValidator();
-        rootValidatorData = hex"";
-        Install[] memory pkgs = new Install[](1);
-        pkgs[0] = Install({moduleType: 1, module: address(rootValidator), moduleData: hex"", internalData: hex""});
-        vm.expectRevert();
-        uups.initialize(pkgs);
     }
 
     function _initialize() internal virtual override {
